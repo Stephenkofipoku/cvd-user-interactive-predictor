907ef8b2c7fdb81fc7758b95cf5ac1cb8495d193
Stephenkofipoku <>
2023-12-10 04:48:19 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,10 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Set up credentials and authorize the Google Sheets API
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
