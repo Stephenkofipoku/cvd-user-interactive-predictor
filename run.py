51924295248eb26330b5ca49dc17dd9827fdc210
Stephen Opoku <stephen.opok@hotmail.com>
2024-01-02 08:32:44 +0000

    Add datasets from google sheet API

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -8,3 +14,10 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
+
+def authorize_google_sheets():
+    """Authorize and return the Google Sheets client."""
+    creds = Credentials.from_service_account_file('creds.json')
+    scoped_creds = creds.with_scopes(SCOPE)
+    gspread_client = gspread.authorize(scoped_creds)
+    return gspread_client
